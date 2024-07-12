221225ba9a2c68d9dc114438e20bf1e1176c3a9a
yoruck2 <yoruck2@gmail.com>
2024-07-12 18:33:01 +0900

    ✨ CenterLabel 구현 가운데 정렬된 label 재사용을 위한 커스텀 UILabel

--- Weather/Main/CustomView/CenterLabel.swift
+++ Weather/Main/CustomView/CenterLabel.swift
@@ -5,4 +5,16 @@
 //  Created by dopamint on 7/12/24.
 //
 
-import Foundation
+import UIKit
+
+class CenterLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        textAlignment = .center
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
