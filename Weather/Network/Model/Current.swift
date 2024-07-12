cc90eac83f2dc590d3237f2a3a43b18f1de2c166
yoruck2 <yoruck2@gmail.com>
2024-07-12 14:04:13 +0900

    ✨ OpenWeather의 Current, Forecast 모델 구현 추후에 불필요한 속성은 삭제 예정

---
+++ Weather/Network/Model/Current.swift
@@ -0,0 +1,70 @@
+//
+//  Weather.swift
+//  Weather
+//
+//  Created by dopamint on 7/11/24.
+//
+
+import Foundation
+
+struct Current: Decodable {
+    let coord: Coord
+    let weather: [Weather]
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let timezone: Int
+    let name: String
+    //    let dt: Int // 시간
+    //    let sys: Sys
+    //    let base: String // internal parameter?
+    //    let cod: Int // internal parameter?
+    
+    struct Weather: Decodable {
+        let id: Int
+        let main: String
+        let description: String
+        let icon: String
+    }
+    
+    struct Coord: Decodable {
+        let lon, lat: Double
+    }
+
+    struct Wind: Codable {
+        let speed: Double
+        let deg: Int
+        let gust: Double
+    }
+
+    struct Clouds: Decodable {
+        let all: Int
+    }
+
+    struct Sys: Decodable {
+        let country: String
+        let sunrise: Int
+        let sunset: Int
+    }
+
+    struct Main: Decodable {
+        let temp: Double
+        let feelsLike: Double
+        let tempMin: Double
+        let tempMax: Double
+        let pressure, humidity, seaLevel, grndLevel: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case temp
+            case feelsLike = "feels_like"
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+            case pressure
+            case humidity
+            case seaLevel = "sea_level"
+            case grndLevel = "grnd_level"
+        }
+    }
+}
+
