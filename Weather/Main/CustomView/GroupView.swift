2c6734ec9e368daacaab886a73bed5bfa0369603
yoruck2 <yoruck2@gmail.com>
2024-07-12 18:32:07 +0900

    ✨ GroupView 구현 비슷한 디자인 재사용을 위한 커스텀 뷰

--- Weather/Main/CustomView/GroupView.swift
+++ Weather/Main/CustomView/GroupView.swift
@@ -5,4 +5,22 @@
 //  Created by dopamint on 7/12/24.
 //
 
-import Foundation
+import UIKit
+
+class GroupView: BaseView {
+    
+    let headerLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        layer.cornerRadius = 15
+        clipsToBounds = true
+        backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+//        alpha = 0.5
+        layer.borderColor = UIColor(named: "darkGray")?.cgColor
+        layer.borderWidth = 1
+        
+    }
+
+}
