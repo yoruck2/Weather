e5c37acbde285594b34c61f75fb107b920b683a5
yoruck2 <yoruck2@gmail.com>
2024-07-13 23:59:03 +0900

    ✨ DetailWeatherInfoCollectionViewCell 추가 임시 데이터추가

--- Weather/Main/CollectionView/DetailWeatherInfoCollectionViewCell.swift
+++ Weather/Main/CollectionView/DetailWeatherInfoCollectionViewCell.swift
@@ -7,6 +7,31 @@
 
 import UIKit
 
-class DetailWeatherInfoCollectionViewCell: UICollectionViewCell {
+class DetailWeatherInfoCollectionViewCell: BaseCollectionViewCell {
     
+    let backView = GroupView()
+    let valueLabel = UILabel()
+    let unitLabel = UILabel()
+    let addtionalInfoLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(backView)
+        backView.addSubview(valueLabel)
+        backView.addSubview(unitLabel)
+        backView.addSubview(addtionalInfoLabel)
+    }
+    
+    override func configureLayout() {
+        backView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        valueLabel.snp.makeConstraints { make in
+            make.top.equalTo(backView.headerLabel.snp.bottom)
+        }
+        
+    }
+    
+    override func configureView() {
+        
+    }
 }
