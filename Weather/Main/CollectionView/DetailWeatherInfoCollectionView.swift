5fdb124c1e7fa72d268810469e1b861a79e37447
yoruck2 <yoruck2@gmail.com>
2024-07-13 23:58:53 +0900

    ✨ DetailWeatherInfoCollectionView 추가 임시 데이터추가

--- Weather/Main/CollectionView/DetailWeatherInfoCollectionView.swift
+++ Weather/Main/CollectionView/DetailWeatherInfoCollectionView.swift
@@ -7,6 +7,16 @@
 
 import UIKit
 
-class DetailWeatherInfoCollectionView: UICollectionView {
+class DetailWeatherInfoCollectionView: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailWeatherInfoCollectionViewCell.id, for: indexPath) as? DetailWeatherInfoCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
 }
