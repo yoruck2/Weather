574f59b84c1c10a9fc61a3a712f483ef5e2f65be
yoruck2 <yoruck2@gmail.com>
2024-07-13 23:59:18 +0900

    ✨ threeHoursForecastCollectionView 추가

--- Weather/Main/CollectionView/ThreeHoursForecastCollectionView.swift
+++ Weather/Main/CollectionView/ThreeHoursForecastCollectionView.swift
@@ -7,6 +7,21 @@
 
 import UIKit
 
-class threeHoursForecastCollectionView: UICollectionView {
+class threeHoursForecastCollectionView: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThreeHoursForecastCollectionViewCell.id, for: indexPath) as? ThreeHoursForecastCollectionViewCell
+        else {
+            return UICollectionViewCell()
+        }
+        
+        cell.hourLabel.text = "8시"
+        cell.weatherIconImageView.image = UIImage(systemName: "star")
+        cell.temperatureLabel.text = "8도"
+        
+        return cell
+    }
 }
