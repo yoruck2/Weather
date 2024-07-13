ede60ca6013b20b1b4eec2ae588fb6ce790f41cd
yoruck2 <yoruck2@gmail.com>
2024-07-13 23:59:29 +0900

    ✨ ThreeHoursForecastCollectionViewCell 추가

--- Weather/Main/CollectionView/ThreeHoursForecastCollectionViewCell.swift
+++ Weather/Main/CollectionView/ThreeHoursForecastCollectionViewCell.swift
@@ -7,6 +7,33 @@
 
 import UIKit
 
-class ThreeHoursForecastCollectionViewCell: UICollectionViewCell {
+class ThreeHoursForecastCollectionViewCell: BaseCollectionViewCell {
     
+    let hourLabel = UILabel()
+    let weatherIconImageView = UIImageView()
+    let temperatureLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(hourLabel)
+        contentView.addSubview(weatherIconImageView)
+        contentView.addSubview(temperatureLabel)
+    }
+    
+    override func configureLayout() {
+        hourLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(contentView.snp.height).multipliedBy(0.3)
+        }
+        weatherIconImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(contentView.snp.height).multipliedBy(0.3)
+        }
+        temperatureLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(contentView.snp.height).multipliedBy(0.3)
+        }
+    }
 }
