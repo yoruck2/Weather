ac3ae9bafb469d7beb0ae899b726deb4f6fc2513
yoruck2 <yoruck2@gmail.com>
2024-07-15 03:17:48 +0900

    ✨ Observable 구현
    MVVM 패턴 적용을 위한 커스텀 Observable

---
+++ Weather/Observable.swift
@@ -0,0 +1,28 @@
+//
+//  Observable.swift
+//  Weather
+//
+//  Created by dopamint on 7/14/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            print("didSet")
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        closure(value)
+        self.closure = closure
+    }
+}
