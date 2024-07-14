9f1487544180cfd846d84e4b46af32aecafa352a
yoruck2 <yoruck2@gmail.com>
2024-07-14 18:36:06 +0900

    ✨ MainViewController ListView Delegate 구현

--- Weather/Main/MainViewController.swift
+++ Weather/Main/MainViewController.swift
@@ -18,5 +18,91 @@ class MainViewController: BaseViewController<MainView> {
         }
     }
     
+    override func configureView() {
+        let flexibleSpaceItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let mapItem = UIBarButtonItem(image: UIImage(systemName: "map"), style: .plain, target: nil, action: nil)
+        let citySearchItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: nil, action: nil)
+        
+        setToolbarItems([mapItem, flexibleSpaceItem, citySearchItem], animated: true)
+        setUpListViews()
+    }
+    
+    func setUpListViews() {
+        rootView.threeHoursForecastCollectionView.delegate = self
+        rootView.threeHoursForecastCollectionView.dataSource = self
+        rootView.threeHoursForecastCollectionView.register(ThreeHoursForecastCollectionViewCell.self,
+                                                           forCellWithReuseIdentifier: ThreeHoursForecastCollectionViewCell.id)
+        rootView.detailWeatherInfoCollectionView.delegate = self
+        rootView.detailWeatherInfoCollectionView.dataSource = self
+        rootView.detailWeatherInfoCollectionView.register(DetailWeatherInfoCollectionViewCell.self,
+                                                           forCellWithReuseIdentifier: DetailWeatherInfoCollectionViewCell.id)
+        
+        rootView.fiveDaysForecastTableView.delegate = self
+        rootView.fiveDaysForecastTableView.dataSource = self
+        rootView.fiveDaysForecastTableView.register(FiveDaysForecastTableViewCell.self,
+                                                    forCellReuseIdentifier: FiveDaysForecastTableViewCell.id)
+    }
 }
-
+// MARK: collectionView -
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch collectionView {
+        case rootView.threeHoursForecastCollectionView:
+            return 10
+        case rootView.detailWeatherInfoCollectionView:
+            return 4
+        default:
+            return 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch collectionView {
+        case rootView.threeHoursForecastCollectionView:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThreeHoursForecastCollectionViewCell.id, 
+                                                                for: indexPath) as? ThreeHoursForecastCollectionViewCell
+            else {
+                return UICollectionViewCell()
+            }
+            cell.hourLabel.text = "8시"
+            cell.weatherIconImageView.image = UIImage(systemName: "star")
+            cell.temperatureLabel.text = "8도"
+            return cell
+        case rootView.detailWeatherInfoCollectionView:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DetailWeatherInfoCollectionViewCell.id,
+                                                                for: indexPath) as? DetailWeatherInfoCollectionViewCell
+            else {
+                return UICollectionViewCell()
+            }
+            cell.addtionalInfoLabel.text = "sd"
+            cell.backView.headerLabel.makeIconLabel(with: "star", text: "star")
+            cell.addtionalInfoLabel.text = "sd"
+            cell.unitLabel.text = "%"
+            cell.addtionalInfoLabel.text = "bbb"
+            return cell
+            
+        default:
+            return  UICollectionViewCell()
+        }
+    }
+}
+// MARK: tableView -
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Weather.FiveDaysForecastTableViewCell.id,
+                                                       for: indexPath) as? FiveDaysForecastTableViewCell 
+        else {
+            return UITableViewCell()
+        }
+        
+        cell.dayLabel.text = "오늘"
+        cell.weatherIconImageView.image = UIImage(systemName: "star")
+        cell.maxTemperatureLabel.text = "최저 -3"
+        cell.minTemperatureLabel.text = "최고 8"
+        return cell
+    }
+}
