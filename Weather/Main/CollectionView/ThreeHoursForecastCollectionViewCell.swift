cdd075cb8d4fe4f16f8d69bd7ab137e42318c5f5
yoruck2 <yoruck2@gmail.com>
2024-07-14 18:33:59 +0900

    💄 ThreeHoursForecastCollectionViewCell UI 구현

--- Weather/Main/CollectionView/ThreeHoursForecastCollectionViewCell.swift
+++ Weather/Main/CollectionView/ThreeHoursForecastCollectionViewCell.swift
@@ -21,18 +21,18 @@ class ThreeHoursForecastCollectionViewCell: BaseCollectionViewCell {
     
     override func configureLayout() {
         hourLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.horizontalEdges.equalToSuperview()
+            make.centerX.equalTo(contentView)
+            make.top.equalTo(contentView).inset(5)
             make.height.equalTo(contentView.snp.height).multipliedBy(0.3)
         }
         weatherIconImageView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.horizontalEdges.equalToSuperview()
+            make.centerX.equalTo(hourLabel)
+            make.top.equalTo(hourLabel.snp.bottom).inset(5)
             make.height.equalTo(contentView.snp.height).multipliedBy(0.3)
         }
         temperatureLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.horizontalEdges.equalToSuperview()
+            make.centerX.equalTo(hourLabel)
+            make.top.equalTo(weatherIconImageView.snp.bottom).inset(5)
             make.height.equalTo(contentView.snp.height).multipliedBy(0.3)
         }
     }
