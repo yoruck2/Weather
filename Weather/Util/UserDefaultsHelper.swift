1534d57552340e249ab735ee6b00126da2696fc7
yoruck2 <yoruck2@gmail.com>
2024-07-15 20:38:03 +0900

    ✨ UserDefaultsHelper 구현 첫 실행 판별과 검색한 도시 저장을 위한 coordinate 저장소 추가

--- Weather/Util/UserDefaultsHelper.swift
+++ Weather/Util/UserDefaultsHelper.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+enum Key: String {
+    case coordinate
+}
+
+final class UserDefaultsHelper {
+    static let standard = UserDefaultsHelper()
+    private init() {}
+    
+    let userDefaults = UserDefaults.standard
+    
+    var coordinate: [String: Any]? {
+        get {
+            return userDefaults.dictionary(forKey: Key.coordinate.rawValue)
+        }
+        set {
+            userDefaults.set(newValue, forKey: Key.coordinate.rawValue)
+        }
+    }
+}
+
