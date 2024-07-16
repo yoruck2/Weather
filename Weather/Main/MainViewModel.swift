fb09f70b25dd6bb78d9073b88a637afebf78dc14
yoruck2 <yoruck2@gmail.com>
2024-07-16 09:33:07 +0900

    ✨ setUpThreeHoursForecast 뷰모델 메서드 구현

--- Weather/Main/MainViewModel.swift
+++ Weather/Main/MainViewModel.swift
@@ -10,7 +10,8 @@ import Foundation
 class MainViewModel {
     let outputCurrentWeather = Observable<Current?>(nil)
     let outputForecast = Observable<Forecast?>(nil)
-//    let outPut
+    let outputThreeHoursForecast = Observable<[Forecast.List]>([])
+    let outputFiveDaysForecast = Observable<[DailyForecast]>([])
     func fetchWeatherData(lat: Double, lon: Double) {
         NetworkService.shared.request(api: .current(lat: lat, lon: lon), model: Current.self) { [weak self] response, error in
             self?.outputCurrentWeather.value = response
@@ -18,7 +19,71 @@ class MainViewModel {
         
         NetworkService.shared.request(api: .forecast(lat: lat, lon: lon), model: Forecast.self) { [weak self] response, error in
             self?.outputForecast.value = response
+            self?.setUpThreeHoursForecast()
+            self?.setUpFiveDaysForecast()
         }
+    }
+    
+    private func setUpThreeHoursForecast() {
+        guard let forecastList = outputForecast.value?.list else {
+            return
+        }
+        
+        let currentDate = Date()
+        let filteredForecast = forecastList.filter { forecastItem in
+            guard let forecastDate = DateFormatter.formatToDate.date(from: forecastItem.dtTxt) else {
+                return false
+            }
+            return forecastDate >= currentDate
+        }
+        
+        let threeDaysForecast = Array(filteredForecast.prefix(24)) // 3일치 데이터 (24 * 3시간 = 72시간)
+        outputThreeHoursForecast.value = threeDaysForecast
+    }
+    
+    private func setUpFiveDaysForecast() {
+        guard let forecastList = outputForecast.value?.list else {
+            return
+        }
+        let groupedForecasts = Dictionary(grouping: forecastList) { forecast -> Date in
+            let date = DateFormatter.formatToDate.date(from: forecast.dtTxt) ?? Date()
+            
+            return Calendar.current.startOfDay(for: date)
+        }
+        
+        let sortedDays = groupedForecasts.keys.sorted()
+        
+        let dailyForecasts = sortedDays.prefix(5).enumerated().map { (index, date) -> DailyForecast in
+            let dayForecasts = groupedForecasts[date] ?? []
+            let maxTemp = dayForecasts.map { $0.main.tempMax }.max() ?? 0
+            let minTemp = dayForecasts.map { $0.main.tempMin }.min() ?? 0
+            let mostCommonWeather = dayForecasts.map { $0.weather.first }.compactMap { $0 }.mostCommon
+            
+            return DailyForecast(date: date,
+                                 isToday: index == 0,
+                                 maxTemp: maxTemp,
+                                 minTemp: minTemp,
+                                 weatherIcon: mostCommonWeather?.icon ?? "")
+        }
+        
+        outputFiveDaysForecast.value = dailyForecasts
     }
 }
 
+struct DailyForecast {
+    let date: Date
+    let isToday: Bool
+    let maxTemp: Double
+    let minTemp: Double
+    let weatherIcon: String
+}
+
+extension Sequence where Element: Hashable {
+    var mostCommon: Element? {
+        return self.grouped().max { $0.1.count < $1.1.count }?.key
+    }
+    
+    func grouped() -> [Element: [Element]] {
+        return Dictionary(grouping: self) { $0 }
+    }
+}
