04a4768ba62d50c5fa1c5cc4af662d7227833947
yoruck2 <yoruck2@gmail.com>
2024-07-18 23:21:47 +0900

    ♻️ DateFormatter 생성을 타입프로퍼티로 변경

--- Weather/extension/DateFormatter+.swift
+++ Weather/extension/DateFormatter+.swift
@@ -9,6 +9,8 @@ import Foundation
 import Kingfisher
 
 extension DateFormatter {
+    
+    static let formatter = DateFormatter()
 //    static func formatDate(_ dateString: String) -> String {
 //        let dateFormatter = DateFormatter()
 //        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
@@ -31,17 +33,30 @@ extension DateFormatter {
     //        return dateFormatter.string(from: date)
     //    }
     //
-    static let formatToDate: DateFormatter = {
-        let formatter = DateFormatter()
+//    static let formatToDate: DateFormatter = {
+//        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+//        formatter.calendar = Calendar(identifier: .iso8601)
+//        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+//        formatter.locale = Locale(identifier: "ko_KR")
+//        print(formatter)
+//        return formatter
+//    }()
+//    
+    static func formatToHour(with date: String) -> String {
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let tempDate = formatter.date(from: date) else {
+            print("날짜 변환 실패: \(date)")
+            return "날짜 오류"
+        }
+        formatter.dateFormat = "H시"
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.locale = Locale(identifier: "ko_KR")
-        return formatter
-    }()
+        let resultDate = formatter.string(from: tempDate)
+        return resultDate
+    }
     
     static let formatToWeekday: DateFormatter = {
-        let formatter = DateFormatter()
         formatter.dateFormat = "E"
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
