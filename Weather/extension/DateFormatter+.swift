db3f7c80be77aef1df9486ba3f5bbe72c722b10a
yoruck2 <yoruck2@gmail.com>
2024-07-21 18:23:39 +0900

    ♻️ DateFormatter 변환 메서드 추가

--- Weather/extension/DateFormatter+.swift
+++ Weather/extension/DateFormatter+.swift
@@ -6,42 +6,11 @@
 //
 
 import Foundation
-import Kingfisher
 
 extension DateFormatter {
     
     static let formatter = DateFormatter()
-//    static func formatDate(_ dateString: String) -> String {
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-//        guard let date = dateFormatter.date (from: dateString) else {
-//            return ""
-//        }
-//        
-//        dateFormatter.dateFormat = "HH:mm"
-//        return dateFormatter.string(from: date)
-//    }
-//    
-//    static func formatDayOfWeek(_ dateString: String) -> String {
-//        let dateFormatter = DateFormatter()
-    //        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    //        guard let date = dateFormatter.date(from: dateString) else {
-    //            return ""
-    //        }
-    //
-    //        dateFormatter.dateFormat = "E"
-    //        return dateFormatter.string(from: date)
-    //    }
-    //
-//    static let formatToDate: DateFormatter = {
-//        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-//        formatter.calendar = Calendar(identifier: .iso8601)
-//        formatter.timeZone = TimeZone(secondsFromGMT: 0)
-//        formatter.locale = Locale(identifier: "ko_KR")
-//        print(formatter)
-//        return formatter
-//    }()
-//    
+
     static func formatToHour(with date: String) -> String {
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         guard let tempDate = formatter.date(from: date) else {
@@ -56,11 +25,43 @@ extension DateFormatter {
         return resultDate
     }
     
-    static let formatToWeekday: DateFormatter = {
+    static func formatToDateOnly(with date: String) -> String {
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        guard let tempDate = formatter.date(from: date) else {
+            print("날짜 변환 실패: \(date)")
+            return "날짜 오류"
+        }
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "ko_KR")
+        let resultDate = formatter.string(from: tempDate)
+        return resultDate
+    }
+    
+    static func formatDateToString(with date: Date) -> String {
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "ko_KR")
+        let resultDate = formatter.string(from: date)
+        return resultDate
+    }  
+    static func formatDateToString(with date: String) -> Date {
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "ko_KR")
+        let resultDate = formatter.date(from: date) ?? Date()
+        return resultDate
+    }
+    
+    static func formatToWeekday(with date: Date) -> String {
         formatter.dateFormat = "E"
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.locale = Locale(identifier: "ko_KR")
-        return formatter
-    }()
+        let resultDate = formatter.string(from: date)
+        return resultDate
+    }
 }
